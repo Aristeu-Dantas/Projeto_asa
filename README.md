de9ea064af43d0849f34daf69d43962fe7e67a6b
Aristeu Dantas <aristeu_dantas@hotmail.com>
2024-08-13 21:21:53 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Projeto_asa
+# Projeto_final_asa
 This project is intended for the ASA - Open Systems Administration course, offered at the Federal Institute of Rio Grande do Norte.
---
+++ run.bat
@@ -0,0 +1,22 @@
+docker stop $(docker ps -q)
+docker rm $(docker ps -a)
+docker rmi $(docker images -a)
+
+docker network rm asa-net
+
+echo "Criando imagens..."
+docker build -t c01 -f Dockerfile.c01 .
+docker build -t c02 -f Dockerfile.c02 .
+docker build -t c03 -f Dockerfile.c03 .
+
+docker build -t proxy -f Dockerfile.proxy .
+docker image ls
+echo "Criando a rede..."
+docker network create -d bridge asa-net
+
+echo "Criando conteiners..."
+docker run -d --net=asa-net --name web01 c01
+docker run -d --net=asa-net --name web02 c02
+docker run -d --net=asa-net --name web03 c03
+
+docker run -dp 80:80 --net=asa-net --name proxy proxy
